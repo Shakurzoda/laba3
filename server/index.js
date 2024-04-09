94ea706fde01dee42136911462ec63cc46e1aa06
roqzych <roqzych@gmail.com>
2024-04-09 15:05:19 +0300

    complete task 1

--- server/index.js
+++ server/index.js
@@ -8,11 +8,35 @@ const app = express();
 const PORT = 3000;
 const router = require("./routes/index");
 
+
+
+function getCurrentDate() {
+  let currentDate = new Date();
+
+  let day = currentDate.getDate();
+  let month = currentDate.getMonth() + 1; // Месяцы в объекте Date начинаются с 0, поэтому добавляем 1
+  let year = currentDate.getFullYear();
+
+  // Добавляем ведущий ноль, если число меньше 10
+  if (day < 10) {
+    day = '0' + day;
+  }
+
+  if (month < 10) {
+    month = '0' + month;
+  }
+
+  return `${day}.${month}.${year}`;
+}
+
 // Функция для выполнения синхронизации
 const performSync = async () => {
   try {
-    const response = await axios.get('https://www.cnb.cz/en/financial_markets/foreign_exchange_market/exchange_rate_fixing/daily.txt?date=27.07.2019');
-    // const data = response.data;
+  
+    // const response = await axios.get('https://www.cnb.cz/en/financial_markets/foreign_exchange_market/exchange_rate_fixing/daily.txt?date=27.07.2019');
+    const response = await axios.get(`https://www.cnb.cz/en/financial_markets/foreign_exchange_market/exchange_rate_fixing/daily.txt?date=${getCurrentDate()}`);
+    const data = response.data;
+    setCurrencyData(data);
     console.log('Синхронизация завершена');
   } catch (error) {
     console.error('An error occurred while fetching data:', error);
@@ -46,7 +70,7 @@ function formatDate(inputDate) {
   return `${day}.${month}.${year}`;
 }
 
-const getCurrencyData = async (textData) => {
+const setCurrencyData = async (textData) => {
   const lines = String(textData).split('\n');
   const dateRegex = /(\d{2} \w+ \d{4})/;
   const date = lines[0].match(dateRegex)[0];
@@ -73,13 +97,23 @@ const getCurrencyData = async (textData) => {
         const dbFile = 'dataBase.json';
         // Читаем текущие данные из файла
         let data = JSON.parse(fs.readFileSync(dbFile));
-        console.log('data -------', data)
-        const updatedData = {dataBase:[...data.dataBase.filter((element) => element.date !== newData.date)]};
+        // console.log('data -------', data)
+        // const updatedData = {dataBase:[...data.dataBase.filter((element) => element.date !== newData.date)]};
         // Добавляем новые данные
-        console.log('updatedData', updatedData);
-        updatedData.dataBase.push(newData);
-        // Записываем обновленные данные обратно в JSON-файл
-        fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
+        // console.log('updatedData', updatedData);
+        if (newData.date && newData.currency && newData.code && newData.rate) {
+          if (!JSON.stringify(data.dataBase).includes(JSON.stringify(newData))) {
+            data.dataBase.push(newData);
+            fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
+
+          }
+          // if (data.dataBase.)
+          // data.dataBase.push(newData);
+          // console.log('newData', newData)
+          // Записываем обновленные данные обратно в JSON-файл
+          // fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
+          // fs.writeFileSync(dbFile, JSON.stringify(data, null, 2));
+        }
       }
     }
   }
@@ -92,7 +126,7 @@ const fillDb = async (date) => {
   // console.log('response = ', response);
   if (response) {
     console.log('typeof response ', typeof response)
-    const data = getCurrencyData(response.data);
+    const data = setCurrencyData(response.data);
   }
 }
 
@@ -101,5 +135,5 @@ app.use("/api", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  fillDb('08.04.2024')
+  // fillDb('08.04.2024')
 });
